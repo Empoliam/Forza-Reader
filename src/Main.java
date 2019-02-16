eb5a5ddf68b87348300fb0ed21d20bd4d6ca5540
Empoliam <liamjwright@btinternet.com>
2019-02-16 20:17:44 +0000

    Added position chart
    Changed from RPM plot to speed plot

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Label;
@@ -21,13 +22,22 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	float RPM;
+	float SPEED;
+	int UNPAUSE;
+	float XPOS;
+	float YPOS;
+	float ZPOS;
+	
 	int MS = 0;
 
+	float maxRecordedSpeed = 10f;
+	
 	DatagramSocket input;
 	Thread T;
 
 	public static void main(String[] args) {
 
+		System.out.println("Starting...");
 		launch(args);
 
 	}
@@ -58,12 +68,20 @@ public class Main extends Application {
 						DatagramPacket packet = new DatagramPacket(buf, 500);
 						input.receive(packet);
 						byte[] currentData = packet.getData();
-						
-						RPM = getBytes(currentData,16,20).getFloat();
-						MS += 16;
 
-						lCounter++;
-						updateMessage(Integer.toString(lCounter));
+						UNPAUSE = getBytes(currentData,0,4).getInt();
+						RPM = getBytes(currentData,16,20).getFloat();
+						SPEED = getBytes(currentData,244,248).getFloat() * 2.237f;
+						XPOS = getBytes(currentData,232,236).getFloat();
+						YPOS = getBytes(currentData,236,240).getFloat();
+						ZPOS = getBytes(currentData,240,244).getFloat();
+
+						if(UNPAUSE == 1) { 
+							MS += 16;
+							lCounter++;
+							updateMessage(Integer.toString(lCounter));
+						}
+						
 						Thread.sleep(16);
 
 					}	
@@ -79,54 +97,81 @@ public class Main extends Application {
 
 			Label rpmLabel = new Label(Float.toString(RPM));
 
-			NumberAxis xAxis = new NumberAxis();
-			NumberAxis yAxis = new NumberAxis();
-			XYChart.Series<Number, Number> series = new Series<>();
-			LineChart<Number, Number> chart = new LineChart<>(xAxis, yAxis);
+			NumberAxis xSpeedAxis = new NumberAxis();
+			NumberAxis ySpeedAxis = new NumberAxis();
+			XYChart.Series<Number, Number> speedSeries = new Series<>();
+			LineChart<Number, Number> speedChart = new LineChart<>(xSpeedAxis, ySpeedAxis);
 
-			chart.getData().add(series);
-			chart.setPrefWidth(500);
-			chart.setPrefHeight(500);
-			chart.setAnimated(false);			
+			speedChart.getData().add(speedSeries);
+			speedChart.setPrefWidth(500);
+			speedChart.setPrefHeight(500);
+			speedChart.setAnimated(false);			
 
 			int maxTime = 60*1000;
 			int timeDivision = 128;
 			int maxPoints = maxTime / 64;
+			
+			xSpeedAxis.setAutoRanging(false);
+			xSpeedAxis.setMinorTickCount(0);
+			xSpeedAxis.setLowerBound(0);
+			xSpeedAxis.setTickUnit(5000);
 
-			xAxis.setAutoRanging(false);
-			xAxis.setMinorTickCount(0);
-			xAxis.setUpperBound((timeDivision * maxPoints) + (timeDivision * 10));
-			xAxis.setTickUnit(5000);
-			xAxis.setLowerBound(timeDivision * 4);
+			speedChart.setCreateSymbols(false);
 
-			chart.setCreateSymbols(false);
+			ySpeedAxis.setAutoRanging(false);
+			ySpeedAxis.setTickUnit(25);
+			ySpeedAxis.setUpperBound(maxRecordedSpeed);
+			ySpeedAxis.setLowerBound(0);
+			
+			NumberAxis xPosAxis = new NumberAxis();
+			NumberAxis yPosAxis = new NumberAxis();
+			XYChart.Series<Number, Number> posSeries = new Series<>();
+			ScatterChart<Number, Number> posChart = new ScatterChart<>(xPosAxis, yPosAxis);
 
-			yAxis.setAutoRanging(false);
-			yAxis.setTickUnit(1000);
-			yAxis.setUpperBound(8000);
-			yAxis.setLowerBound(0);
+			posChart.getData().add(posSeries);
+			posChart.setPrefWidth(500);
+			posChart.setPrefHeight(500);
+			posChart.setAnimated(false);			
+			
+			
+			xSpeedAxis.setAutoRanging(true);
+
+			speedChart.setCreateSymbols(false);
+
+			ySpeedAxis.setAutoRanging(true);
 
 
 			bgLoop.messageProperty().addListener((observable, old, updated) -> {
 
 				rpmLabel.setText(Float.toString(RPM) + " RPM");	
 
+				if(maxRecordedSpeed < SPEED) {
+					maxRecordedSpeed = SPEED;
+					ySpeedAxis.setUpperBound(maxRecordedSpeed);
+				}
+				
 				if(MS % timeDivision == 0) {
-					series.getData().add(new XYChart.Data<Number, Number>(MS, RPM));
-					if(series.getData().size() > maxPoints) {
-						series.getData().remove(0, series.getData().size() - maxPoints);
-						xAxis.setLowerBound(xAxis.getLowerBound() + timeDivision);
-						xAxis.setUpperBound(xAxis.getUpperBound() + timeDivision);
+					
+					speedSeries.getData().add(new XYChart.Data<Number, Number>(MS, SPEED));
+					if(speedSeries.getData().size() > maxPoints) {
+						speedSeries.getData().remove(0, speedSeries.getData().size() - maxPoints);
+						xSpeedAxis.setLowerBound(xSpeedAxis.getLowerBound() + timeDivision);
+						xSpeedAxis.setUpperBound(xSpeedAxis.getUpperBound() + timeDivision);
 
 					}
+					xSpeedAxis.setUpperBound(xSpeedAxis.getUpperBound() + timeDivision);
+					
+					posSeries.getData().add(new XYChart.Data<Number, Number>(XPOS, ZPOS));
+					
 				}
+				
 			});
 
 			VBox mainBox = new VBox();	
 			mainBox.setPadding(new Insets(10));
 			mainBox.setMinWidth(200d);
 
-			mainBox.getChildren().addAll(rpmLabel, chart);
+			mainBox.getChildren().addAll(rpmLabel, speedChart, posChart);
 
 			Scene mainScene = new Scene(mainBox);
 			mainStage.setScene(mainScene);
